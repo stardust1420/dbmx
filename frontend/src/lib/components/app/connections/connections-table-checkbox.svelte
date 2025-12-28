9532add14be771cfcb1b81f035b557b88e9ba0ee
Karan Singhasane <ksinghasane14@gmail.com>
2025-12-29 01:22:57 +0530

    add table to view and edit existing connections

---
+++ frontend/src/lib/components/app/connections/connections-table-checkbox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+  import type { ComponentProps } from "svelte";
+  let {
+    checked = false,
+    onCheckedChange = (v?: boolean) => (checked = v),
+    ...restProps
+  }: ComponentProps<typeof Checkbox> = $props();
+</script>
+<div class="flex items-center justify-center mr-2">
+  <Checkbox bind:checked={() => checked, onCheckedChange} {...restProps} />
+</div>
