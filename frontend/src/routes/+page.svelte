3bd2ed6538ed6b257d6febe14d78b6dd42d9877d
Karan Singhasane <ksinghasane14@gmail.com>
2026-04-09 17:20:44 +0530

    sidebar redesign complete

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,10 +8,12 @@
 	import Plus from '@lucide/svelte/icons/plus';
 	import Ellipsis from '@lucide/svelte/icons/ellipsis';
 	import Paperclip from '@lucide/svelte/icons/paperclip';
+	import * as Tooltip from "$lib/components/ui/tooltip/index.js";
 
 	import { Textarea } from '$lib/components/ui/textarea/index.js';
 	import * as Kbd from '$lib/components/ui/kbd/index.js';
 	import { Badge } from '$lib/components/ui/badge/index.js';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	// Active tab properties
 	let tabID = $state(0);
@@ -48,13 +50,8 @@
 		}
 	}
 
-	// Right Sidebar
-	let collapsed = $state(false);
-	let chatPane: ReturnType<typeof Resizable.Pane>;
-	let chatPaneSize = $state(0);
-
 	function toggleChatPane() {
-		if (collapsed) {
+		if (chatPaneCollapsed) {
 			chatPane.expand();
 			if (chatPaneSize > 0) {
 				chatPane.resize(chatPaneSize);
@@ -67,10 +64,15 @@
 			chatPane.collapse();
 		}
 	}
+
+	// Right Sidebar
+	let chatPaneCollapsed: boolean = $state(false);
+	let chatPane: ReturnType<typeof Resizable.Pane>;
+	let chatPaneSize: number = $state(0);
 </script>
 
 <Resizable.PaneGroup direction="horizontal">
-	<Resizable.Pane defaultSize={100}>
+	<Resizable.Pane defaultSize={100} class="mr-1">
 		<Sidebar.Provider>
 			<AppSidebar
 				bind:tabID
@@ -97,6 +99,7 @@
 					bind:orderBy
 					bind:groupBy
 					bind:tableColumns
+					bind:chatPaneCollapsed
 					{toggleChatPane}
 				/>
 			</Sidebar.Inset>
@@ -107,19 +110,19 @@
 	
 	<Resizable.Pane
 		defaultSize={0}
-		maxSize={40}
+		maxSize={50}
 		collapsible={true}
 		collapsedSize={0}
-		onCollapse={() => (collapsed = true)}
-		onExpand={() => (collapsed = false)}
+		onCollapse={() => (chatPaneCollapsed = true)}
+		onExpand={() => (chatPaneCollapsed = false)}
 		bind:this={chatPane}
-		class="flex h-full"
+		class="flex rounded-lg bg-neutral-900 my-2"
 	>
-		<div class="mx-1 my-2 flex w-full flex-col">
+		<div class="flex w-full h-full flex-col">
 			<!-- header -->
-			<div class="flex flex-[1] items-center justify-between">
+			<div class="flex flex-[1] items-center justify-between m-1">
 				<SquarePen size={18} class="m-2" />
-				<p>Chat</p>
+				<p>Stardust AI</p>
 				<X size={18} class="m-2" onclick={toggleChatPane} />
 			</div>
 			<div class="flex flex-[20] items-center justify-center">
@@ -133,48 +136,23 @@
 					</p>
 				</div>
 			</div>
-			<div class="ml-2 flex w-full flex-[1] items-center justify-start">
-				<Badge class="m-1 my-2 bg-green-700 text-white">
-					<p class="font-medium font-normal">Explain</p>
-				</Badge>
-				<Badge class="m-1 my-2 bg-blue-700 text-white">
-					<p class="font-medium font-normal">Optimise</p>
-				</Badge>
-				<Badge class="m-1 my-2 bg-red-700 text-white">
-					<p class="font-medium font-normal">Fix Syntax</p>
-				</Badge>
-			</div>
 			<div
-				class="bg-secondary/40 h-42 flex flex-[5] flex-col items-center justify-center rounded-xl border p-2"
+				class="flex flex-[1] flex-col items-center justify-center rounded-t-3xl bg-black"
 			>
-				<div class="m-2 flex h-36 w-full flex-[3] items-center justify-start">
-					<div class=" flex h-11 items-center justify-center rounded-lg">
-						<Badge
-							variant="secondary"
-							class="mr-2 flex h-11 items-center justify-center rounded-xl bg-gray-800 pr-0 pt-0"
-						>
-							<p class="font-medium font-normal">Line 10-13</p>
-							<X size={15} color="red" class="mb-1 ml-1 self-start rounded-full hover:bg-red-700" />
-						</Badge>
-					</div>
-					<div class=" flex h-11 items-center justify-center rounded-lg">
-						<Badge
-							variant="secondary"
-							class="mr-2 flex h-11 items-center justify-center rounded-lg bg-gray-800 pr-0 pt-0"
-						>
-							<p class="font-medium font-normal">transaction</p>
-
-							<X size={14} color="red" class="mb-1 ml-1 self-start rounded-full hover:bg-red-700" />
-						</Badge>
-					</div>
-				</div>
-				<div class="flex h-24 max-h-24 w-full flex-[3] items-center justify-center">
-					<Textarea value="Explain" class="h-24 max-h-24 bg-black" placeholder="Ask anything..." />
+				<div class="flex w-full flex-[5] items-center justify-center">
+					<Textarea class="max-h-48 m-1 focus-visible:ring-0 border-0" placeholder="Ask anything..." />
 				</div>
 				<div class="flex w-full flex-[1] items-end justify-start">
-					<Plus size={16} class="m-2" />
-					<Paperclip size={16} class="m-2" />
-					<Ellipsis size={16} class="m-2" />
+				<Tooltip.Provider>
+					<Tooltip.Root>
+						<Tooltip.Trigger>
+							<Button variant="ghost"><Plus size={12} /></Button>
+						</Tooltip.Trigger>
+						<Tooltip.Content>
+							<p>Add context</p>
+						</Tooltip.Content>
+					</Tooltip.Root>
+				</Tooltip.Provider>
 				</div>
 			</div>
 		</div>
