8a658e87658a20aa174640f298e79ada781c53b0
Karan Singhasane <ksinghasane14@gmail.com>
2026-08-23 01:20:31 +0530

    enhance theme toggle

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -18,6 +18,10 @@
 
 	import {isLoggedIn, postgresConnectionsMap, userFullName, userEmail, userAvatar, userUseDefaultKey, userIsAIEnabled, editorFontSize } from '$lib/state.svelte';
 	import { GetLoggedInUser } from '$lib/wailsjs/go/app/Auth';
+	import { toggleMode, mode } from "mode-watcher";
+	import MoonIcon from "@lucide/svelte/icons/moon";
+	import SunIcon from "@lucide/svelte/icons/sun";
+
 
 	// Fetch all connections when the root layout is mounted
 	// This is mounted only for the root layout, so it will only run once
@@ -148,6 +152,21 @@
 			<Command.Item onSelect={() => goToRoute('/llm_manager')}>
 				<BotIcon class="mr-2 size-4" />
 				<span>LLM Manager</span>
+				<Command.Shortcut>⌘S</Command.Shortcut>
+			</Command.Item>
+			<Command.Item onclick={toggleMode}>
+				<SunIcon
+					class="mr-2 h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+				/>
+				<MoonIcon
+					class="mr-2 absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+				/>
+				Theme - 
+				{#if mode.current == "light"}
+					Light
+				{:else}
+					Dark
+				{/if} 
 				<Command.Shortcut>⌘S</Command.Shortcut>
 			</Command.Item>
 		</Command.Group>
